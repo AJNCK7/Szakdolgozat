72049bc5064a1eb3bd9601b71ad807694cef21c9
AJNCK7 <61760397+AJNCK7@users.noreply.github.com>
2021-11-02 11:33:48 +0100

    Fejléc dropdown menü elemekkel

--- orarendtervezo/src/app/app.module.ts
+++ orarendtervezo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -40,6 +41,7 @@ const Matimports: any[] = [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
+    BrowserAnimationsModule,
     [...Matimports]
   ],
   providers: [...Matimports],
