bdded45b6f6d7d1819cda44a34a1fb5025ef44e1
AJNCK7 <61760397+AJNCK7@users.noreply.github.com>
2021-11-24 12:51:45 +0100

    Regisztráció összekötése a firebaseval, megerősítő email újraküldő implementálása

--- orarendtervezo/src/app/app-routing.module.ts
+++ orarendtervezo/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserVerifyEmailComponent } from './user_management/user_verify_email/user_verify_email.component';
 import { UserRegisterComponent } from './user_management/user_register/user_register.component';
 import { UserLoginComponent } from './user_management/user_login/user_login.component';
 import { HomepageComponent } from './commons/homepage/homepage.component';
@@ -7,7 +8,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home_page', component: HomepageComponent},
   { path: 'user_login', component: UserLoginComponent},
-  { path: 'user_register', component: UserRegisterComponent}
+  { path: 'user_register', component: UserRegisterComponent},
+  { path: 'user_verify_email_address', component: UserVerifyEmailComponent }
 ];
 
 @NgModule({
