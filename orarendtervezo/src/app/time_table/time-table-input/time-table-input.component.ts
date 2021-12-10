0eee10568d63f78b500ca23822837e2fa1a59a9d
AJNCK7 <61760397+AJNCK7@users.noreply.github.com>
2021-12-10 10:11:07 +0100

    Órarend táblás előnézet implementálása

--- orarendtervezo/src/app/time_table/time-table-input/time-table-input.component.ts
+++ orarendtervezo/src/app/time_table/time-table-input/time-table-input.component.ts
@@ -2,6 +2,27 @@ import { Component} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
+export interface TimeTableInputInterface {
+  ID: number;
+  SUBJECT_NAME: string;
+  DAY: string;
+  SUBJECT_WEIGHT: string;
+  SUBJECT_START_TIME: string;
+  SUBJECT_END_TIME: string;
+  SAME_SUBJECT: string;
+  CLASSROOM: string;
+  TEACHER: string;
+  CREDIT: number;
+  PRIORITY: number;
+  COLOR: string;
+}
+
+const ELEMENT_DATA: TimeTableInputInterface[] = [
+  {ID: 1, SUBJECT_NAME: 'Magprog', DAY: "Monday", SUBJECT_WEIGHT: 'Mandatoriy', SUBJECT_START_TIME: '10:40', 
+  SUBJECT_END_TIME: '11:50', SAME_SUBJECT: 'Asd', CLASSROOM: 'C/111', TEACHER: 'Troll', CREDIT: 1, PRIORITY: 9, COLOR: 'Red'}
+];
+
+
 @Component({
   selector: 'app-time-table-input',
   templateUrl: './time-table-input.component.html',
@@ -36,6 +57,10 @@ export class TimeTableInputComponent{
       ]]
     })
   }
+
+  displayedColumns: string[] = ['id', 'subjectName', 'day', 'subjectWeight', 'classStartTime', 
+                                'classEndTime', 'sameSubject', 'classroom',  'teacher', 'credit', 'priority', 'color'];
+  dataSource = ELEMENT_DATA;
 
   get timeTableInputGet() {
     return this.form.controls;
