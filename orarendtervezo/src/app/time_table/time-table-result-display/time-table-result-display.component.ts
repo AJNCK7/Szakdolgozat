2b3f8d7f8aada7808cdfdb89027b24c71142837b
AJNCK7 <61760397+AJNCK7@users.noreply.github.com>
2021-12-30 09:41:25 +0100

    Órarendtervező tábla generálás eredmény megjelenítés - alap tábla felépítés, css-el

--- orarendtervezo/src/app/time_table/time-table-result-display/time-table-result-display.component.ts
+++ orarendtervezo/src/app/time_table/time-table-result-display/time-table-result-display.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeTableResultDisplayComponent implements OnInit {
 
+  days: string[] = ['DAYS_SHORT_FORM.MONDAY', 'DAYS_SHORT_FORM.TUESDAY', 'DAYS_SHORT_FORM.WEDNESDAY', 
+                    'DAYS_SHORT_FORM.THURSDAY', 'DAYS_SHORT_FORM.FRIDAY', 'DAYS_SHORT_FORM.SATURDAY', 'DAYS_SHORT_FORM.SUNDAY'];
+  hours: string[] = ['7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
+
   constructor() { }
 
   ngOnInit(): void {
